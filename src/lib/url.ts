3bed4d2eded82029748e12e8d97b3e70f9de4dde
thracian141 <58891220+thracian141@users.noreply.github.com>
2024-04-29 02:10:03 +0300

    1.2.2

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,2 +1,2 @@
-// export let url = "https://localhost:5000";
-export let url = "http://atraversari-001-site1.ctempurl.com/"
+//export let url = "https://localhost:5000";
+export let url = "https://atraversari-001-site1.ctempurl.com/"
